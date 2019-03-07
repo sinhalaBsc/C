fa1f649ff9b37e2ddb6d313a26ff6adeacf41778
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2019-03-07 23:47:25 +0300

    Add files via upload

---
+++ 006/foo.c
@@ -0,0 +1,22 @@
+/*
+tutorial Array - collection of data
+*/
+
+#include <stdio.h> 
+
+int main(int argc, char * argv[]){
+  
+  int a[4];
+  int b[4]={1,2,3,4};
+
+  a[0]=5;
+  a[1]=6;
+  a[2]=7;
+  a[3]=8;
+
+  printf("a value are %d %d %d %d\n",a[0],a[1],a[2],a[3]);
+  printf("b value are %d %d %d %d\n",b[0],b[1],b[2],b[3]);
+
+
+  return 0;
+}
