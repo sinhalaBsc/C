b1d8090ab78bb96fd3c76fa90cb4384218309f22
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2019-03-07 20:44:59 +0300

    Add files via upload

---
+++ 002/foo.c
@@ -0,0 +1,25 @@
+/*
+tutorial - int and char data type 
+*/
+
+#include <stdio.h>
+
+
+int main(int argc ,char *argv[]){
+
+  int k1,k2,sum;
+  char mychar;
+
+  k1=12;
+  k2=8;
+  sum=k1+k2;
+  printf("k1 value is %d\n",k1);
+  printf("k2 value is %d\n",k2);
+  printf("sum is k1 + k2 =%d\n",sum);
+  
+  mychar='f';
+  printf("Char number value is %c \n",mychar);
+
+ return 0;
+}
+
