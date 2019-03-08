2303d33b4f35daa69fe0c204900383e8cf833cff
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2019-03-08 11:52:42 +0300

    Add files via upload

---
+++ 008_for-loop/foo.c
@@ -0,0 +1,23 @@
+/*
+tutorial - for loop
+*/
+
+#include <stdio.h>
+
+int main(int argc , char * argv[]){
+
+  int i;
+  
+  for(i=0;i<10;i++){
+    printf("i value is %d\n", i);
+   }
+
+
+  // 'j' varialbe define inside for loop brackets
+  for(int j=0;j<10;j++){
+    printf("j value is %d\n", j); 
+  }
+
+
+ return 0;
+}
