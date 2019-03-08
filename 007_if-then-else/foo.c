4e40bd514bb8f733907a49d6ed643f6377d7caa1
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2019-03-08 10:12:15 +0300

    Add files via upload

---
+++ 007_if-then-else/foo.c
@@ -0,0 +1,29 @@
+/*
+tutorial - if else statments
+*/
+
+#include <stdio.h>
+
+int main(int argc, char * argv[]){
+
+ int num=5;
+
+ // if statments 
+ if(num>10){
+    printf("number %d greter than 10\n",num);
+ }
+
+ if(num<10){
+    printf("number %d less than 10\n",num);
+ }
+
+ // if else statments 
+ if(num>10){
+    printf("number %d greter than 10\n",num);
+ }else{
+    printf("number %d less than 10\n",num);
+ }
+
+ return 0;
+
+}
